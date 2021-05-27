58fdb2ee9dfdee86d2362cc25641c0e387b06902
zdoroven <45197696+zdoroven@users.noreply.github.com>
2021-05-27 11:43:33 +0300

    - работа с ключами

--- cripta3/Keys.cs
+++ cripta3/Keys.cs
@@ -15,51 +15,80 @@ namespace cripta3
 
         private const string KEYS_FILE = "keys.txt";
 
-        public static void Write(RSAParameters rsaClose, RSAParameters rsaOpen, byte[] aesKey, byte[] aesIV)
+        private const string AES_KEY_FILE = "AesKey.txt";
+        private const string AES_IV_FILE = "AesIV.txt";
+        private const string RSA_PUB_KEY_FILE = "RSAPublic.txt";
+        private const string RSA_PRI_KEY_FILE = "RSAPrivate.txt";
+
+        public static void Write(string rsaClose, string rsaOpen, byte[] aesKey, byte[] aesIV)
         {
-            using (FileStream fileStream = new FileStream(KEYS_FILE, FileMode.OpenOrCreate))
+            using (FileStream fileStream = new FileStream(RSA_PRI_KEY_FILE, FileMode.OpenOrCreate))
             {
-                // Write the data to the file, byte by byte.
-                for (int i = 0; i < dataArray.Length; i++)
-                {
-                    fileStream.WriteByte(dataArray[i]);
-                }
-
-                for (int i = 0; i < dataArray.Length; i++)
-                {
-                    fileStream.WriteByte(dataArray[i]);
-                }
-
-
-                // Set the stream position to the beginning of the file.
-                // fileStream.Seek(0, SeekOrigin.Begin);
-
-                // Read and verify the data.
-                /*for (int i = 0; i < fileStream.Length; i++)
-                {
-                    if (dataArray[i] != fileStream.ReadByte())
-                    {
-                        Console.WriteLine("Error writing data.");
-                        return;
-                    }
-                }
-                Console.WriteLine("The data was written to {0} " +
-                    "and verified.", fileStream.Name);*/
+                var bytes = Encoding.UTF8.GetBytes(rsaClose);
+                fileStream.Write(bytes, 0, bytes.Length);
             }
+            using (FileStream fileStream = new FileStream(RSA_PUB_KEY_FILE, FileMode.OpenOrCreate))
+            {
+                var bytes = Encoding.UTF8.GetBytes(rsaOpen);
+                fileStream.Write(bytes, 0, bytes.Length);
+            }
+            using (FileStream fileStream = new FileStream(AES_KEY_FILE, FileMode.OpenOrCreate))
+            {
+                fileStream.Write(aesKey, 0, aesKey.Length);
+            }
+            using (FileStream fileStream = new FileStream(AES_IV_FILE, FileMode.OpenOrCreate))
+            {
+                fileStream.Write(aesIV, 0, aesIV.Length);
+            }
+            //                using (FileStream fileStream = new FileStream(KEYS_FILE, FileMode.OpenOrCreate))
+            //          {
+            // Write the data to the file, byte by byte.
+            /*for (int i = 0; i < dataArray.Length; i++)
+            {
+                fileStream.WriteByte(dataArray[i]);
+            }
+
+            for (int i = 0; i < dataArray.Length; i++)
+            {
+                fileStream.WriteByte(dataArray[i]);
+            }
+            */
+
+            // Set the stream position to the beginning of the file.
+            // fileStream.Seek(0, SeekOrigin.Begin);
+
+            // Read and verify the data.
+            /*for (int i = 0; i < fileStream.Length; i++)
+            {
+                if (dataArray[i] != fileStream.ReadByte())
+                {
+                    Console.WriteLine("Error writing data.");
+                    return;
+                }
+            }
+            Console.WriteLine("The data was written to {0} " +
+                "and verified.", fileStream.Name);*/
+            //}
         }
 
         public static string[] Read()
         {
             string[] result = new string[4];
             string line = "";
-            int i = 0;
-
-            using (StreamReader sr = new StreamReader(KEYS_FILE))
+            var arr = new string[4];
+            arr[0] = RSA_PUB_KEY_FILE;
+            arr[1] = RSA_PRI_KEY_FILE;
+            arr[2] = AES_KEY_FILE;
+            arr[3] = AES_IV_FILE;
+            for (int i = 0; i < 4; i++)
             {
-                while ((line = sr.ReadLine()) != null)
+                using (StreamReader sr = new StreamReader(arr[i]))
                 {
-                    result[i] = line.Split(':')[1];
-                    i++;
+                    if ((line = sr.ReadLine()) != null)
+                        result[i] = line;
+                    else
+                        result[i] = "";
+                    line = "";
                 }
             }
             return result;
--- cripta3/Program.cs
+++ cripta3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.IO;
 using System.Text;
 
@@ -7,34 +8,64 @@ namespace cripta3
 {
     class Program
     {
-        private const string DOC_FILE = "doc.txt";
-        private const string ENC_DOC_FILE = "encryptedDoc.txt";
-        private const string DEC_DOC_FILE = "decryptedDoc.txt";
+        private const string DOC_FILE = "..\\..\\..\\doc.txt";
+        private const string ENC_DOC_FILE = "..\\..\\..\\encryptedDoc.txt";
+        private const string DEC_DOC_FILE = "..\\..\\..\\decryptedDoc.txt";
 
         static void Main(string[] args)
         {
             /* Читаем текст для последующего шифрования */
             var text = FileReadWriter.ReadString(DOC_FILE);
-
             /* Используем AES для шифрования текста */
+            // Пееменные для ключей
+            var aesKey = Array.Empty<Byte>();
+            var aesIV = Array.Empty<Byte>();
+            RSAParameters rsaOpen;
+            RSAParameters rsaClose;
+
             var aes = new AES();
-
-            aes.GenerateKeys();
-
-            var encryptedBytes = aes.EncryptStringToBytes_Aes(text, aes.AesKey, aes.AesIV);
-
-            /* Пишем в файл зашифрованный текст */
-            FileReadWriter.WriteString(ByteArrayToString(encryptedBytes), ENC_DOC_FILE);
-
-            /* Шифруем саенсовые ключи AES с помощью RSA */
             var rsa = new RSA();
-            rsa.GenerateKeys();
 
-            var encAesKey = RSA.RSAEncrypt(aes.AesKey, rsa.PublicKey, false);
-            var encAesKeyIV = RSA.RSAEncrypt(aes.AesIV, rsa.PublicKey, false);
+            // Читаем старые ключи, если есть
+            var keys = Keys.Read();
+            if(keys[0] == "")
+            {
+                // Нет сохраненных ключей, надо генерить новые
+                aes.GenerateKeys();
+                aesKey = aes.AesKey;
+                aesIV = aes.AesIV;
+                rsa.GenerateKeys();
+                rsaOpen = rsa.PublicKey;
+                rsaClose = rsa.PrivateKey;
+            }
+            else
+            {
+                
+                aesKey = Encoding.ASCII.GetBytes(keys[2]);
+                aesIV = Encoding.ASCII.GetBytes(keys[3]);
+            }
+            
+            
+            var encryptedBytes = aes.EncryptStringToBytes_Aes(text, aesKey, aesIV);
+            Console.WriteLine("Изначальный текст: " + text + "\n");
+            /* Пишем в файл зашифрованный текст */
+            //FileReadWriter.WriteString(ByteArrayToString(encryptedBytes), ENC_DOC_FILE);
+            Console.WriteLine("Зашифрованный текст: " + ByteArrayToString(encryptedBytes) + "\n");
+;            /* Шифруем саенсовые ключи AES с помощью RSA */
+
+            var encAesKey = RSA.RSAEncrypt(aesKey, rsa.PublicKey, false);
+            var encAesKeyIV = RSA.RSAEncrypt(aesIV, rsa.PublicKey, false);
 
             /* Записываем шифрованные сеансовые ключи AES и ключи RSA в файл */
-            Keys.Write(rsa.PrivateKey)
+            //Keys.Write(rsa.PrivateKey);
+
+            // Расшифровываем ключи симметричного алгоритма
+            var AesKey = RSA.RSADecrypt(encAesKey, rsa.PrivateKey, false);
+            var AesIV = RSA.RSADecrypt(encAesKeyIV, rsa.PrivateKey, false);
+            // Расшифровываем текст
+            var decryptedText = aes.DecryptStringFromBytes_Aes(encryptedBytes, AesKey, AesIV);
+            //FileReadWriter.WriteString(decryptedText, DEC_DOC_FILE);
+            Console.Write("Расшифрованный текст: " + decryptedText + "\n");
         }
 
         private static string ByteArrayToString(byte[] bytes)
